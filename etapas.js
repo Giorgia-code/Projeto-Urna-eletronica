0ec12e074e057ff73c41b61b1877f822b396e0b0
Giorgia Maia <g.isa.maia@hotmail.com>
2021-11-24 17:27:45 -0300

    Adicionando projeto de urna eletronica

---
+++ etapas.js
@@ -0,0 +1,46 @@
+let etapas = [
+  {
+    titulo: 'VEREADOR',
+    numeros: 5,
+    candidatos: [
+      {
+        numero: '12098',
+        nome: 'Sabotage',
+        partido: 'Musicalidade',
+        fotos: [{ url: 'Vereador.jpg', legenda: 'Vereador' }]
+      },
+      {
+        numero: '34765',
+        nome: 'Mano Brown',
+        partido: 'Mcs na veia',
+        fotos: [{ url: 'Vereador2.jpg', legenda: 'Vereador' }]
+      }
+    ]
+  },
+  {
+    titulo: 'PREFEITO',
+    numeros: 2,
+    candidatos: [
+      {
+        numero: '12',
+        nome: 'Rick',
+        partido: 'Ciencia',
+        vice: 'Baby Yoda',
+        fotos: [
+          { url: 'Prefeito.jpg', legenda: 'Prefeito' },
+          { url: 'Vice.jpg', legenda: 'Vice', small: true }
+        ]
+      },
+      {
+        numero: '66',
+        nome: 'MC Terror',
+        partido: 'Medonhos',
+        vice: 'Vampirinha',
+        fotos: [
+          { url: 'Prefeito2.jpg', legenda: 'Prefeito' },
+          { url: 'Vice2.jpg', legenda: 'Vice', small: true }
+        ]
+      }
+    ]
+  }
+]
